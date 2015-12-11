97ea8455001c9a364ebf42edf5924a4c5dc7c91e
ctevnan <carolyntevnan@yahoo.com>
2015-12-10 20:40:07 -0500

    initial commit

---
+++ pnz.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+  $("gamebuttons").on("click", function(){
+    var bid = $ (this).attr("id")
+}
+
+var userScore = 0;
+var enemyScore = 0;
+var rounds = 0;
+
+$(document).ready(function(){
+
+  $(".gamebuttons").on("click", function()
+
+    uhhhhhh
