f44092f5c8940ff58a364f9fff7b6b2ca4fde740
ctevnan <carolyntevnan@yahoo.com>
2015-12-15 18:35:02 -0500

    pushing html and js changes. made my modal work but buttons need more work

--- pnz.js
+++ pnz.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+
+
 var myWords = ["Pirate", "Ninja", "Zombie"];
 
   $("#changeWordBtn").on("click", function() {
@@ -60,30 +62,6 @@ var userScore = 0;
 var enemyScore = 0;
 var round = 0;
 
-
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-  
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
 
 //return a random string from an array
 
