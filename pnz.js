aa2277b656749fc7ce5612e7e8ea0f3c78c884f0
ctevnan <carolyntevnan@yahoo.com>
2016-02-08 18:27:57 -0500

    switched around some buttons. changed the primary button to a reset button, changed button block to be the primary button (it lost functionality ie is no longer showing p, n or z randomly, but I plan on fixing that.

--- pnz.js
+++ pnz.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var randomWords = ["Pirate", "Ninja", "Zombie"];
 
- $("#changeWordButton").on("click", function() {
+ $("#btn-primary").on("click", function() {
     var randomNumber = Math.floor(Math.random() * randomWords.length);
    $("h1").fadeIn(250, function() {
      $(this).html(randomWords[randomNumber]);
