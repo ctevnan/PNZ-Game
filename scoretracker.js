c0db6e50433c8b6024db7e7b8b2c721cfee9153b
ctevnan <carolyntevnan@yahoo.com>
2016-02-08 19:17:59 -0500

    modified scoretracker.js so it would connect to gamelogic.js

--- scoretracker.js
+++ scoretracker.js
@@ -1,15 +1,17 @@
-$(document).ready(function () {
-  var gameState = {
+window.gamelogic = {
+  gameState = {
     userScore: 0,
     computerScore: 0,
     roundCount: 1
   }
-  
-  $(".btn-info").on("click", function () {
+}
+
+$(document).ready(function () {
+
+  $(".btn-info").on("click", function() {
     var tagName = $(this).data("tag");
     gameState[tagName]++;
 
     $("#" + tagName).html(gameState[tagName]);
   });
-
 });    
