1a9b3efc9fa4b7a3e33f3a8e7d2089b5bae94229
ctevnan <carolyntevnan@yahoo.com>
2016-02-13 21:20:38 -0500

    changed jquery file

--- pnzjquery.js
+++ pnzjquery.js
@@ -5,46 +5,55 @@ $('.choice-btn').on("click", function() {
    var computerChoice = buttonChoices[Math.floor(Math.random() * buttonChoices.length -1) +1]
    var userChoice = this.getAttribute('data-choice');
    console.log("on click, fire fire fire");
-
-      //gameLogic(userChoice, computerChoice);
+   console.log("user: " + userChoice);
+   console.log("computer: " + computerChoice);
+    
+    // gameLogic(userChoice, computerChoice);
      //for wins
-     if (userChoice === "pirate" && choices[computerChoice] === "ninja")  {
+     if (userChoice === "pirate" && computerChoice === "ninja")  {
        // pirateNinja ();
        // computerWin();
        console.log("logic fired");
-     } else if (userChoice === "pirate" && choices[computerChoice] === "zombie") {
+     } else if (userChoice === "pirate" && computerChoice === "zombie") {
       //pirateZombie ();
       //userWin ();
-    } else if (userChoice === "pirate" && choices [computerChoice] === "pirate") {
+      console.log("logic fired");
+    } else if (userChoice === "pirate" && computerChoice === "pirate") {
       //ninjaPirate ();
       //userWin ();
-    } else if (userChoice === "ninja" && choices [computerChoice] === "zombie") {
+      console.log("logic fired");
+    } else if (userChoice === "ninja" && computerChoice === "zombie") {
       //ninjaZombie();
       //computerWin();
-    } else if (userChoice === "zombie" && choices [computerChoice] === "pirate") {
+      console.log("logic fired");
+    } else if (userChoice === "zombie" && computerChoice === "pirate") {
       //zombiePirate();
       //computerWin();
-    } else if (userChoice === "zombie" && choices [computerChoice] === "ninja")  {
+      console.log("logic fired");
+    } else if (userChoice === "zombie" && computerChoice === "ninja")  {
       //zombieNinja();
       //userWin();
-    } else if (userChoice === "pirate" && choices [computerChoice] === "pirate") {
+      console.log("logic fired");
+    } else if (userChoice === "pirate" && computerChoice === "pirate") {
       //piratePirate();
       //tie();
-    } else if (userChoice === "ninja" && choices [computerChoice] === "ninja")  {
+      console.log("logic fired");
+    } else if (userChoice === "ninja" && computerChoice === "ninja")  {
       //ninjaNinja();
       //tie();
-    } else if (userChoice === "zombie" && choices [computerChoice] === "zombie") {
+      console.log("logic fired");
+    } else if (userChoice === "zombie" && computerChoice === "zombie") {
       //zombieZombie();
       //tie();
+      console.log("logic fired");
     } else {
-           //console.log("logic defaulted missing case?");
+           console.log("logic defaulted missing case?");
     }
-
 })       
 
 $('.choice-btn').on("click", function() {
    var userChoice = this.getAttribute("data-choice");
- })
+  })
 
  var pirateNinja = function() {
      $("#user").removeClass("https://cdn4.iconfinder.com/data/icons/SIGMA/networking/png/64/piracy.png https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-64.png https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");         
@@ -106,4 +115,4 @@ $('.choice-btn').on("click", function() {
       $("#computer").removeClass("https://cdn4.iconfinder.com/data/icons/SIGMA/networking/png/64/piracy.png https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-64.png https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");
       $("#computer").addClass("https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");
   };
-});  
+ 
