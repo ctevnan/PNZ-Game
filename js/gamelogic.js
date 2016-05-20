c09b6ad7591fc390e764a5b705ba9a9d4e3dd5bb
ctevnan <carolyntevnan@yahoo.com>
2016-05-20 15:01:08 -0400

    working on js, game logic.

--- js/gamelogic.js
+++ js/gamelogic.js
@@ -1,26 +1,37 @@
-$(document).ready(function() {
-  var buttonChoices = ["pirate", "ninja", "zombie"];
+//declare global vars
+var playerScore = 0;
+var computerScore = 0;
+var roundCount = 1;
+var playerForWins;
+var playerGoChoose = '';
+var computerGoChoose = '';
+
+var computerStrings = [
+  'pirate',
+  'ninja',
+  'zombie'
+];
 
   function randomButton() {
-    var randomIndex = Math.floor(Math.random() * buttonChoices.length);
-    $(".btn-primary").html(buttonChoices[randomIndex]).attr("data-selection", randomIndex);
+    var randomIndex = Math.floor(Math.random() * computerStrings.length);
+    $(".btn-primary").html(computerStrings[randomIndex]).attr("data-selection", randomIndex);
   }
  
   $(".btn-primary").on("click", function() {
-    var computerChoice = Math.floor(Math.random() * buttonChoices.length);
-    var userChoice = parseInt($(this).attr("data-selection"));
+    var computerGoSelect = Math.floor(Math.random() * computerStrings.length);
+    var playerGoSelect = parseInt($(this).attr("data-selection"));
 
         if(computerChoice === userChoice) {
           $(".modal-body").html("Same choice. No Points.");
-        } else if(computerChoice === 0 && userChoice === 1) {
+        } else if(computerGo === 0 && playerGo === 1) {
           //Computer gets a point
           $(".modal-body").html("Computer wins!");
           window.gamelogic.gameState.computerScore++;
-        } else if(computerChoice === 1 && userChoice === 0) {
+        } else if(computerGoSelect === 1 && playerGoSelect === 0) {
           //User gets a point
         } $(".modal-body").html("User wins!");
 
-          window.gamelogic.gameState.userScore++;
+          window.gamelogic.gameState.playerScore++;
         })
         $("#myModal").modal("show");
         window.gamelogic.gameState.roundCount++;
@@ -28,20 +39,18 @@ $(document).ready(function() {
         $("span").each(function() {
           $(this).html(window.gamelogic.gameState[$(this).attr("id")]);
         });
-
       var randomButton();
     });
-
     var randomButton();
   ;  
 
 
  function bindControls() {
    $(".btn-primary").on("click", function() {
-      var userChoice = $(this).attr("data-choice");
-      console.log(userChoice);
-      var computerChoice = Math.floor(Math.random() * choices.length);
-      console.log(choices[computerChoice]);
+      var playerGoSelect = $(this).attr("data-choice");
+      console.log(userGoSelect);
+      var computerGoSelect = Math.floor(Math.random() * choices.length);
+      console.log(choices[computerGoSelect]);
       window.gameLogic(userChoice, computerChoice);
   });
 };
@@ -68,7 +77,124 @@ function roundIncrement() {
 
  };
 };
+$('#myActiveGamescoreModal').modal("hide");
+  var buttonChoices = ["pirate", "ninja", "zombie"];
 
+$('.choice-btn').on("click", function() {
+   var computerChoice = buttonChoices[Math.floor(Math.random() * buttonChoices.length -1) +1]
+   var userChoice = this.getAttribute('data-choice');
+   console.log("on click, fire fire fire");
+   console.log("user: " + userChoice);
+   console.log("computer: " + computerChoice);
+    
+    function gameLogic(userChoice, computerChoice) {
+     //for wins
+     if (userChoice === "pirate" && computerChoice === "ninja")  {
+        pirateNinja();
+        computerWin();
+       console.log("logic fired");
+     } else if (userChoice === "pirate" && computerChoice === "zombie") {
+        pirateZombie();
+        userWin();
+      console.log("logic fired");
+    } else if (userChoice === "pirate" && computerChoice === "pirate") {
+        ninjaPirate();
+        userWin();
+      console.log("logic fired");
+    } else if (userChoice === "ninja" && computerChoice === "zombie") {
+        ninjaZombie();
+        computerWin();
+      console.log("logic fired");
+    } else if (userChoice === "zombie" && computerChoice === "pirate") {
+        zombiePirate();
+        computerWin();
+      console.log("logic fired");
+    } else if (userChoice === "zombie" && computerChoice === "ninja")  {
+        zombieNinja();
+        userWin();
+      console.log("logic fired");
+    } else if (userChoice === "pirate" && computerChoice === "pirate") {
+        piratePirate();
+        tie();
+      console.log("logic fired");
+    } else if (userChoice === "ninja" && computerChoice === "ninja")  {
+        ninjaNinja();
+        tie();
+      console.log("logic fired");
+    } else if (userChoice === "zombie" && computerChoice === "zombie") {
+        zombieZombie();
+        tie();
+      console.log("logic fired");
+    } else {
+           console.log("logic defaulted missing case?");
+    }
+}       
+
+$('.choice-btn').on("click", function() {
+   var userChoice = this.getAttribute("data-choice");
+  })
+
+ var pirateNinja = function() {
+     $("#user").removeClass("https://cdn4.iconfinder.com/data/icons/SIGMA/networking/png/64/piracy.png https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-64.png https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");         
+      $("#user").addClass("https://cdn4.iconfinder.com/data/icons/SIGMA/networking/png/64/piracy.png");
+      $("#computer").removeClass("https://cdn4.iconfinder.com/data/icons/SIGMA/networking/png/64/piracy.png https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-64.png https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");
+      $("#computer").addClass("https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-64.png"); 
+  };
+  var pirateZombie = function() {
+      $("#user").removeClass("https://cdn4.iconfinder.com/data/icons/SIGMA/networking/png/64/piracy.png https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-64.png https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");
+      $("#user").addClass("https://cdn4.iconfinder.com/data/icons/SIGMA/networking/png/64/piracy.png");
+      $("#computer").removeClass("https://cdn4.iconfinder.com/data/icons/SIGMA/networking/png/64/piracy.png https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-64.png https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");
+      $("#computer").addClass("https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");
+  };
+  var ninjaPirate = function() {
+      $("#user").removeClass("https://cdn4.iconfinder.com/data/icons/SIGMA/networking/png/64/piracy.png https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-64.png https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");
+      $("#user").addClass("https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-64.png");
+      $("#computer").removeClass("https://cdn4.iconfinder.com/data/icons/SIGMA/networking/png/64/piracy.png https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-64.png https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");
+      $("#computer").addClass("https://cdn4.iconfinder.com/data/icons/SIGMA/networking/png/64/piracy.png");
+  };
+
+  var ninjaZombie = function() {
+      $("#user").removeClass("https://cdn4.iconfinder.com/data/icons/SIGMA/networking/png/64/piracy.png https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-64.png https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");
+      $("#user").addClass("https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-64.png");
+      $("#computer").removeClass("https://cdn4.iconfinder.com/data/icons/SIGMA/networking/png/64/piracy.png https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-64.png https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");
+      $("#computer").addClass("https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");
+  };
+
+  var zombiePirate = function() {
+      $("#user").removeClass("https://cdn4.iconfinder.com/data/icons/SIGMA/networking/png/64/piracy.png https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-64.png https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");
+      $("#user").addClass("https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");
+      $("#computer").removeClass("https://cdn4.iconfinder.com/data/icons/SIGMA/networking/png/64/piracy.png https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-64.png https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");
+      $("#computer").addClass("https://cdn4.iconfinder.com/data/icons/SIGMA/networking/png/64/piracy.png");
+  };
+
+  var zombieNinja = function() {
+      $("#user").removeClass("https://cdn4.iconfinder.com/data/icons/SIGMA/networking/png/64/piracy.png https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-64.png https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");
+      $("#user").addClass("https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");
+      $("#computer").removeClass("https://cdn4.iconfinder.com/data/icons/SIGMA/networking/png/64/piracy.png https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-64.png https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");
+      $("#computer").addClass("https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-64.png");
+  };
+
+  var piratePirate = function() {
+      $("#user").removeClass("https://cdn4.iconfinder.com/data/icons/SIGMA/networking/png/64/piracy.png https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-64.png https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");
+      $("#user").addClass("https://cdn4.iconfinder.com/data/icons/SIGMA/networking/png/64/piracy.png");
+      $("#computer").removeClass("https://cdn4.iconfinder.com/data/icons/SIGMA/networking/png/64/piracy.png https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-64.png https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");
+      $("#computer").addClass("https://cdn4.iconfinder.com/data/icons/SIGMA/networking/png/64/piracy.png");
+  };
+
+  var ninjaNinja = function() {
+      $("#user").removeClass("https://cdn4.iconfinder.com/data/icons/SIGMA/networking/png/64/piracy.png https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-64.png https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");
+      $("#user").addClass("https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-64.png");
+      $("#computer").removeClass("https://cdn4.iconfinder.com/data/icons/SIGMA/networking/png/64/piracy.png https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-64.png https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");
+      $("#computer").addClass("https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-64.png");
+ };
+
+  var zombieZombie = function() {
+      $("#user").removeClass("https://cdn4.iconfinder.com/data/icons/SIGMA/networking/png/64/piracy.png https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-64.png https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");
+      $("#user").addClass("https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");
+      $("#computer").removeClass("https://cdn4.iconfinder.com/data/icons/SIGMA/networking/png/64/piracy.png https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-64.png https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");
+      $("#computer").addClass("https://cdn3.iconfinder.com/data/icons/halloween-free-set/512/Halloween-17-64.png");
+};
+});     
 function userWin() {
     PNZ.gameState.userScore++;
     console.log(PNZ.gameState.userScore)
@@ -127,3 +253,18 @@ window.PNZ = {
     roundCount: 1
   }
 };
+
+window.gameLogic = 
+  gameState {
+
+    userScore: 0,
+    computerScore: 0,
+    roundCount: 1
+}
+
+  $(".btn-info").on("click", function() {
+    var tagName = $(this).data("tag");
+    window.gamelogic.gameState[tagName]++;
+
+    $("#" + tagName).html(window.gamelogic.gameState[tagName]);
+  }); 
